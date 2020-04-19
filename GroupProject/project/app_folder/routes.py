f36648c7cf7fff2b260906c768593b7d7fdceed0
Charles-Philip <60630499+Charles-Philip@users.noreply.github.com>
2020-04-19 14:17:06 -0700

    Logout, errorhandler(404), and login manager added

--- GroupProject/project/app_folder/routes.py
+++ GroupProject/project/app_folder/routes.py
@@ -5,7 +5,9 @@ from app_folder.forms import LoginForm
 from .forms import RegistrationForm
 from .models import User
 from flask import flash
+from flask import logout_user, url_for, login
 
+@app.route('/index')
 @app.route('/')
 def home():
     myname = 'carlos' 
@@ -17,6 +19,7 @@ def login():
     form_curr = LoginForm()
     if form_curr.validate_on_submit():
         flash('Hello you logged in! Welcome to your schedule')
+        login_user(current_user) # CPI
     return render_template('login.html', form=form_curr)
     
 
@@ -31,3 +34,23 @@ def reg():
         db.session.commit()
         return redirect('/')
     return render_template('register.html', form=form)
+
+@app.route('/logout', methods=['GET','POST']) #CPI
+def logout():
+    if not confirm_login():
+        flash('You are currently not logged in!')
+        return redirect(url_for('/'))
+    if logout_user():
+        flash('You have successfully logged out!')
+        return redirect(url_for('/'))
+    abort(404)
+    
+@app.errorhandler(404) #CPI
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+app.register_error_handler(404, page_not_found)
+
+
+
+
