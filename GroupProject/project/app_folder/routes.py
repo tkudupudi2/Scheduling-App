a35631e109a1d527f72f2cdbae87bd5d73f2ce19
Tanav Kudupudi <tkudupudi2@gmail.com>
2020-04-18 23:03:19 -0500

    Added some basic html code for the login page ; Also changed the home page ; Try to add CSS but I couldn't get the link to work

--- GroupProject/project/app_folder/routes.py
+++ GroupProject/project/app_folder/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, redirect
 from app_folder.forms import LoginForm
 from .forms import RegistrationForm
 from .models import User
+from flask import flash
 
 @app.route('/')
 def home():
@@ -11,10 +12,13 @@ def home():
     mylist = ['carlos', 'jane', 'alex']
     return render_template('index.html', name=myname, names=mylist)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form_curr = LoginForm()
+    if form_curr.validate_on_submit():
+        flash('Hello you logged in! Welcome to your schedule')
     return render_template('login.html', form=form_curr)
+    
 
 @app.route('/register', methods=['GET', 'POST'])
 def reg():
