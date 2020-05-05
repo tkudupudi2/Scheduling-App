5ca04b57cd8013dbdd7b917d9184d20293243943
Charles-Philip <60630499+Charles-Philip@users.noreply.github.com>
2020-05-04 23:40:24 -0700

    Revert "starting from scratch"
    This reverts commit 9fb4d280b5c995557e13a1bf4e4f4ab235a36757.

---
+++ GroupProject/project/app_folder/models.py
@@ -0,0 +1,23 @@
+from app_folder import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int (id))
+
+def get_name(name) :
+    user = user.query.filter_by(username = name).first_or_404(description = 'There is no data associated with {}'.format(username))
+    return render_template('show_user.html', user = user)
