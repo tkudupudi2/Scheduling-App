9fb4d280b5c995557e13a1bf4e4f4ab235a36757
Arash Bahramian <arash.bahramiand@gmail.com>
2020-05-04 22:55:15 -0700

    starting from scratch
    I'm an idiot

--- GroupProject/project/app_folder/generate_calendar.py
+++
@@ -1,3 +0,0 @@
-import calendar
-hc = calendar.HTMLCalendar(calendar.THURSDAY)
-print(hc.formatmonth(2020,5))
---
+++ GroupProject/project/run.py
@@ -0,0 +1,5 @@
+from app import apps
+
+if __name__ == '__main__':
+    apps.run(debug=True)
+    
