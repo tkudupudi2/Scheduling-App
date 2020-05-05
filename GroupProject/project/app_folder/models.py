27d6642dca388c365675f82aa0f137c58350dc79
Arash Bahramaindehkordi <arash.bahramiand@gmail.com>
2020-05-04 21:33:41 -0700

    made a Table for availability results(for guest.html)

--- GroupProject/project/app_folder/models.py
+++ GroupProject/project/app_folder/models.py
@@ -1,9 +1,11 @@
-from app_folder import db, login
 from datetime import datetime
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask_table import Table, Col, LinkCol
 
-'''Database for the username, email, password, and availability'''
+#Creates a user database with id, username, email, and hashed password columns
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
@@ -13,30 +15,39 @@ class User(db.Model, UserMixin):
     availability_end = db.Column(db.String(64))
     length = db.Column(db.Integer)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    #the user is related to the post that they post
 
-    '''This generates a hash for the password'''
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-    
-    '''This will check the password hash'''
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    '''The username is saved'''
     def __repr__(self):
         return f'<user: {self.username}>'
 
-'''Users have the option to upload their post which is saved into the database'''
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+#User can upload post and it gets into the database
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(256))
     timestamps = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
+
     def __repr__(self):
         return f'<post: {self.body}>'
 
-'''Provides a link to the users'''
+
+# for guest to see appointments
+class Results(Table):
+    id = Col('Id', show=False)
+    username = Col('User')
+    email = Col('Email')
+    availability_start = Col('Availability Start')
+    availability_end = Col('Availability Ends')
+    length = Col('Length')
+   # edit = LinkCol('Edit', 'edit', url_kwargs=dict(id='id'))
+
+#Provides a link to the users table since flask-login doesnt have access to database
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
